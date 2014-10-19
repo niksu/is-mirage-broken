3ff3234b8b0c197411aec1e94e56b2fa6202cc37
Anil Madhavapeddy <anil@recoil.org>
2014-10-18 17:49:33 -0700

    tweak

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ script=$2
 repo=avsm/docker-opam
 docker=docker.io
 
-${docker} run -v `pwd`/scripts:/scripts -t $repo:$tag sh -c "cd /scripts && ./run.sh $script"
+${docker} run -v `pwd`/scripts:/scripts -t $repo:$tag sh -c "cd /scripts && ./$script"
