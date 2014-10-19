4d88aa0640bff0cd10502c010e21fbcc590847e4
Anil Madhavapeddy <anil@recoil.org>
2014-10-19 08:58:44 -0700

    preserve cwd

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ script=$2
 repo=avsm/docker-opam
 docker=docker.io
 
-${docker} run -v `pwd`/scripts:/scripts -t $repo:$tag sh -c "cd /scripts && ./$script"
+${docker} run -v `pwd`/scripts:/scripts -t $repo:$tag sh -c "/scripts/$script"
