22d822553e2e0e58c9cb24c00b6bd2ecfbe1de6e
Anil Madhavapeddy <anil@recoil.org>
2014-10-19 12:51:52 -0700

    split out mirage-skeleton and mirage-www into two scripts

--- cron.sh
+++ cron.sh
@@ -1,9 +1,14 @@
 #!/bin/sh -x
 
-for script in mirage-dev; do
-  for distro in ubuntu-trusty; do
+git pull
+
+for script in mirage-www mirage-skeleton; do
+  for distro in ubuntu-trusty centos-7; do
     for ver in 4.01.0 4.02.1; do
       ./build.sh $distro-$ver $script | tee logs/$script-$distro-$ver
     done
   done
 done
+
+git add logs/
+git commit -m "Sync logs"
