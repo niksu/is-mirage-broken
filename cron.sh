7f9ce11ef185f0d69281a7907a6fe0044a99d842
Anil Madhavapeddy <anil@recoil.org>
2014-10-19 08:56:38 -0700

    tee logs

--- cron.sh
+++ cron.sh
@@ -3,14 +3,7 @@
 for script in mirage-dev; do
   for distro in ubuntu-trusty; do
     for ver in 4.01.0 4.02.1; do
-      ./build.sh $distro-$ver $script > logs/$script-$distro-$ver
-      if [ $? = 0 ]; then
-        rm -f BROKEN-$script-$distro-$ver
-        ln -nsf logs/$script-$distro-$ver WORKS-$script-distro-ver
-      else
-        rm -f WORKS-$script-$distro-$ver
-        ln -nsf logs/$script-$distro-$ver BROKEN-$script-distro-ver
-      fi
+      ./build.sh $distro-$ver $script | tee logs/$script-$distro-$ver
     done
   done
 done
