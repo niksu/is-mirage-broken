f462ed0f566e3cb6c2634cd5b7d57fbd1b62a4ac
Anil Madhavapeddy <anil@recoil.org>
2014-10-19 17:56:51 -0700

    delete intermediate containers to avoid leaking space

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ script=$2
 repo=avsm/docker-opam
 docker=docker.io
 
-${docker} run -v `pwd`/scripts:/scripts -t $repo:$tag sh -c "/scripts/$script"
+${docker} run --rm=true -v `pwd`/scripts:/scripts -t $repo:$tag sh -c "/scripts/$script"
