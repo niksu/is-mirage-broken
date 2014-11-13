d98f3536e3e2813b676fab5fdb99401b7a83f169
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2014-11-13 01:43:19 +0000

    switched from using hub to using create_pull_request -- a tool that currently resides in https://github.com/avsm/opam-sync-github-prs/pull/3 modified to update the mirage-www stubs introduced in https://github.com/mirage/mirage-www/pull/235

--- update_mirage-www.sh
+++ update_mirage-www.sh
@@ -9,30 +9,20 @@
 # Nik Sultana, Cambridge University Computer Lab, November 2014
 #
 # NOTE
-# - The "hub" tool is used here to interact with GitHub.
-# - We rely on "hub" to have access to user credentials via the GITHUB_USER and
-#   GITHUB_PASSWORD environment variables.
-# - Credentials are passed to "git" differently -- here I assume that we're
-#   using GIT_ASKPASS.
-#   FIXME would be useful to use single source of credentials -- such as RSA
-#   keys.
+# - An ocaml-github tool is used here to interact with GitHub.
+#   CREATE_PULL_REQUEST must point to the create_pull_request tool.
+# - GITHUB_USER is the GitHub account under which we'd like to create the PR --
+#   for instance, this could be the account of the build bot.
 # - We rely on IS_MIRAGE_BROKEN_DIR to contain the path within which we'll keep
-#   a clone of GITHUB_USER's fork of mirage/mirage-www.
+#   a clone of our fork of mirage/mirage-www. We assume that this repo will
+#   have a particular remote set: the GITHUB_USER remote should point to
+#   GITHUB_USER's fork.
+# - For git operations, such as "git push", we rely on contextually-available
+#   credentials that git can use. These could be supplied via GIT_ASKPASS, etc.
+# - In that clone, we assume that "origin" tracks mirage/mirage-www, while the
+#   remote called GITHUB_USER tracks GITHUB_USER/mirage-www.
 
-EXPECTED_GITHUB_USER=bactrian
-
-if [ ! "${GITHUB_USER}" == "${EXPECTED_GITHUB_USER}" ]
-then
-  echo "Expecting GITHUB_USER to be set to ${EXPECTED_GITHUB_USER}." >&2
-  exit 1
-fi
-
-if [ -z "${GITHUB_PASSWORD}" || -z "${GIT_ASKPASS}" ]
-then
-  echo "Expecting GITHUB_PASSWORD and GIT_ASKPASS to be set to the password of
-  ${GITHUB_USER}'s GitHub account." >&2
-  exit 1
-fi
+GITHUB_USER=bactrian
 
 # Our location matters: make sure that IS_MIRAGE_BROKEN_DIR is defined, and
 # changed directory to it.
@@ -46,33 +36,20 @@ fi
 
 cd "${IS_MIRAGE_BROKEN_DIR}"
 
-if [ -z "$(which hub)" ]
-then
-  echo "Missing dependency: hub" >&2
-  echo "Can be downloaded from http://hub.github.com/" >&2
-  exit 1
-fi
-
 if [ ! -d mirage-www ]
 then
   echo "Did not find a copy of ${GITHUB_USER}'s fork of mirage-www.
-  Forking and cloning it."
-
-  # To use hub we first clone mirage-www, then we fork it using hub, and we
-  # obtain a "remote" to GITHUB_USER's fork of mirage-www.
-  git clone git://github.com/mirage/mirage-www
-  cd mirage-www
-  hub fork --no-remote
-  git remote add "${GITHUB_USER}" "https://${GITHUB_USER}@github.com/mirage/mirage-www"
-else
-  cd mirage-www
+  Please fork and clone it into ${IS_MIRAGE_BROKEN_DIR}."
+  exit 1
 fi
+
+cd mirage-www
 
 echo "We seem to have a copy of ${GITHUB_USER}'s fork of mirage-www.
 Making sure it's up to date."
 git fetch origin master
 git checkout master
-# Bring bactrian's fork up to date.
+# Bring GITHUB_USER's fork up to date.
 # This should always be a fast-forward merge -- should never fail.
 git merge origin/master
 git push "${GITHUB_USER}"
@@ -82,8 +59,8 @@ git push "${GITHUB_USER}"
 # NOTE diff uses exit status 1 to indicate that the two files differ, but
 #  a non-zero exit status will cause the whole script to fall (because of -e).
 #  We avoid this by using the "&& true" suffix below.
-CHECK_DIFF=$(diff -q <(sed -e '1d' ../logs/README.md) \
-  <(sed -e '1d' tmpl/wiki/is_mirage_broken.md)) && true
+CHECK_DIFF=$(diff -q <(sed -e '2d' ../logs/README.md) \
+  <(sed -e '2d' tmpl/wiki/is_mirage_broken.md)) && true
 if [[ ! ${CHECK_DIFF} =~ "differ" ]]
 then
   echo "Build statuses haven't changed since last time we ran."
@@ -102,26 +79,32 @@ else
 
   cp ../logs/README.md tmpl/wiki/is_mirage_broken.md
   perl -ne '
-    if ($_ =~ /^(.*)date.+(\(\*NOTE is-mirage-broken:marker\*\))/)
+    if ($_ =~ /^(.*)date.+$/)
     {
       my ($minute, $hour, $day, $month, $year) = (localtime)[1,2,3,4,5];
       printf ("%sdate (%04d, %02d, %02d, %02d, %02d) %s\n", $1,
-        $year + 1900, $month + 1, $day, $hour, $minute, $2);
+        $year + 1900, $month + 1, $day, $hour, $minute);
     } else {
       print "$_";
-    }' src/data.ml > "${TMPFILE}"
-  cp "${TMPFILE}" src/data.ml
+    }' src/bactrian.ml > "${TMPFILE}"
+  cp "${TMPFILE}" src/bactrian.ml
 
   MSG="updated build status at $(date);"
 
   # FIXME hardcoded paths -- also elsewhere in code.
-  git commit tmpl/wiki/is_mirage_broken.md src/data.ml \
+  git commit tmpl/wiki/is_mirage_broken.md src/bactrian.ml \
     -m "${MSG}"
   git push "${GITHUB_USER}"
-  # make PR from bactrian/master to mirage-www/master
-  hub pull-request -m "${MSG}" \
-    -b mirage/mirage-www:master \
-    -h "${GITHUB_USER}:master"
+  # make PR from GITHUB_USER/master to mirage-www/master
+  # NOTE instead of making a PR from GITHUB_USER/master, we could fork off the
+  # current HEAD of mirage-www/master and create a new branch under
+  # GITHUB_USER/mirage-www, and PR off that; but then we'd need to prune stale
+  # branches.
+  ${CREATE_PULL_REQUEST} -m "${MSG}" \
+    -t "The current answer to the question: is-mirage-broken?" \
+    -x ${GITHUB_USER} -r mirage-www -b master \
+    -x mirage -r mirage-www -b master \
+    -u ${GITHUB_USER} -h master -k infra
 fi
 
 cd ..
